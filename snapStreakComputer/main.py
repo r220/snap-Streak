8bf56af9e1212e4b7a3ba26c491617ec52dda118
reem <upontime000@gmail.com>
2024-03-19 22:42:56 +0300

    add py + png files to rep

---
+++ snapStreakComputer/main.py
@@ -0,0 +1,76 @@
+import pyautogui as pag
+import time
+pag.FAILSAFE = True
+
+def get_position(image: str):
+    try:
+        position = pag.locateCenterOnScreen(image, confidence=0.7)
+        if position is None:
+            print(f'{image} not found on screen')
+            return None
+        else:
+            x = position[0] / 2
+            y = position[1] / 2
+            if image != "quit1.png":
+                return x,y
+            else:
+                return x - 22, y - 22
+
+    except OSError as e:
+        print(f'there is no such file as {image}')
+        raise Exception(e)
+
+def click(image: str):
+    position = get_position(image)
+    pag.moveTo(position, duration=0.2)
+    pag.click(position)
+    print(position, "OPENED !!!\n")
+
+def select_users():
+    names = ["reemas_r35", "RAHAF", "D I N A", "re.zh97", "SHAHAD", "Jawaher", "Riri"]
+    for name in names:
+        pag.write(name)
+        click("block.png")
+        click("delete.png")
+        click("search.png")
+
+if __name__ == '__main__':
+
+    #while pag.FAILSAFE:
+
+        # 1- open SNAPCHAT
+        click("app.png")
+        pag.sleep(15)
+
+        # 2- click snap button twice
+        try:
+            click("snap2.png")
+        except pag.ImageNotFoundException:
+            try:
+                click("snap1.png")
+                pag.sleep(6)
+                click("snap2.png")
+            except pag.ImageNotFoundException:
+                click("snap2.png")
+
+        pag.sleep(0.15)
+
+        # -- text --
+        click("text.png")
+        pag.write("this is a snap from my computer machine !")
+        pag.sleep(0.2)
+
+        # 3- click 'send to' button
+        click("sendTo.png")
+        pag.sleep(0.3)
+
+        # 4- detect friends with fire sign
+        click("search.png")
+        select_users()
+
+        # 5- SEND
+        click("finalSend.png")
+
+        # 6- close SNAPCHAT
+        pag.moveTo(get_position("quit1.png"), duration=0.2)
+        pag.click(get_position("quit1.png"))
